0525741043dec6619d42e50fcd8e37ca390f384a
blablanumerodeux <blablanumerodeux@gmail.com>
2020-04-23 22:11:40 -0400

    develop: PUT request

--- src/app/shelters/shelter/shelter.component.ts
+++ src/app/shelters/shelter/shelter.component.ts
@@ -1,5 +1,9 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Shelter} from '../../models/Shelter.models';
+import {ShelterList} from '../../models/ShelterList.models';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {environment} from '../../../environments/environment';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'shelter',
@@ -11,14 +15,36 @@ export class ShelterComponent implements OnInit {
   @Input() shelter: Shelter;
   edit: boolean = false;
 
-  constructor() {
+  sheltersList: Shelter[];
+  profileForm = new FormGroup({
+    address: new FormControl(''),
+    availableBeds: new FormControl(''),
+  });
+  private uri = 'api/v1/shelters';
+  private url = environment.baseUrl + '/' + this.uri;
+
+  constructor(private _httpClient: HttpClient) {
   }
 
   ngOnInit(): void {
+    this.profileForm.get('address').setValue(this.shelter.address);
+    this.profileForm.get('availableBeds').setValue(this.shelter.availableBeds);
   }
 
   onSubmit() {
-    console.log('tt');
-    // TODO: PUT request
+    this.edit = !this.edit;
+    this.shelter.address = this.profileForm.value.address;
+    this.shelter.availableBeds = this.profileForm.value.availableBeds;
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': 'Basic ' + btoa('admin:nimda')
+      })
+    };
+    this._httpClient.put<ShelterList>(this.url + '/' + this.shelter.id, this.shelter, httpOptions)
+      .subscribe((sheltersList: ShelterList) => {
+          this.sheltersList = sheltersList.content;
+        }
+      );
   }
 }
